97ff6bcd10db86e20d1c532fdd8d360a3c7dea53
glebkaaa <129597452+glebkaaa@users.noreply.github.com>
2023-05-01 01:14:56 +0600

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,6 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
 
 
-@bot.command()
-async def show(ctx):
-    await ctx.send('Бот умеет выдавать ответы по тематикам!' + '\n' + '$mem - случайный мем' + '\n' + '$fox - случайное фото лисы' + '\n' + '$author - авторский мем' + '\n' + '$anime - случайное название аниме')
-
-
 def get_img():
     url = 'https://randomfox.ca/floof/'
     res = requests.get(url)
@@ -32,6 +27,39 @@ def get_anime():
     return title_ru
 
 
+def common_image():
+    global common_picture
+    image = choice(os.listdir('images/common'))
+    with open(f'images/common/{image}', 'rb') as f:
+        common_picture = discord.File(f)
+
+
+def rare_image():
+    global rare_picture
+    image = choice(os.listdir('images/rare'))
+    with open(f'images/rare/{image}', 'rb') as f:
+        rare_picture = discord.File(f)
+
+
+def epic_image():
+    global epic_picture
+    image = choice(os.listdir('images/epic'))
+    with open(f'images/epic/{image}', 'rb') as f:
+        epic_picture = discord.File(f)
+
+
+def legendary_image():
+    global legendary_picture
+    image = choice(os.listdir('images/legendary'))
+    with open(f'images/legendary/{image}', 'rb') as f:
+        legendary_picture = discord.File(f)
+
+
+@bot.command()
+async def show(ctx):
+    await ctx.send('Бот умеет выдавать ответы по тематикам!' + '\n' + '$mem - случайный мем' + '\n' + '$fox - случайное фото лисы' + '\n' + '$author - авторский мем' + '\n' + '$anime - случайное название аниме')
+
+
 @bot.command()
 async def fox(ctx):
     fox_image = get_img()
@@ -42,15 +70,6 @@ async def fox(ctx):
 async def anime(ctx):
     anime_link = get_anime()
     await ctx.send(anime_link)
-    
-
-
-@bot.command()
-async def mem(ctx):
-    image = choice(os.listdir('images'))
-    with open(f'images/{image}', 'rb') as f:
-        picture = discord.File(f)
-    await ctx.send(file=picture)
 
 
 @bot.command()
@@ -58,5 +77,26 @@ async def author(ctx):
     with open('images/my-meme.jpg', 'rb') as f:
         picture = discord.File(f)
     await ctx.send(file=picture)
+
+
+@bot.command()
+async def mem(ctx):
+    rarity = randint(1, 4)
+    if rarity == 1:
+        common_image()
+        await ctx.send(file=common_picture)
+        await ctx.send('Обычный мем')
+    elif rarity == 2:
+        rare_image()
+        await ctx.send(file=rare_picture)
+        await ctx.send('Редкий мем')
+    elif rarity == 3:
+        epic_image()
+        await ctx.send(file=epic_picture)
+        await ctx.send('Эпический мем')
+    else:
+        legendary_image()
+        await ctx.send(file=legendary_picture)
+        await ctx.send('Легендарный мем')
 
 bot.run(bot_token)
